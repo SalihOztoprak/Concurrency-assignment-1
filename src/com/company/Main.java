c0052c407f69cbf64aee94f262f490948b9196ce
Salih Oztoprak <salih.58@live.nl>
2018-09-05 14:30:42 +0200

    Begin phase assignment 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,19 +10,22 @@ public class Main {
 
     }
 
-    public void run(){
-        ArrayList<Integer>randomList = new ArrayList<>();
-        randomList=generateNumbers(25000);
-        selectionSort(randomList);
-        System.out.println(randomList.size());
+    public void run() {
+        ArrayList<Integer> randomList;
+        randomList = generateNumbers(25000);
+        randomList = selectionSort(randomList);
+
+        System.out.println(randomList);
+        System.out.println("the size of the list ; "+randomList.size());
+        System.out.println("is the list sorted: "+ isSorted(randomList));
     }
 
 
-    private ArrayList<Integer> generateNumbers(int amount){
+    private ArrayList<Integer> generateNumbers(int amount) {
         ArrayList<Integer> numberList = new ArrayList<>();
         Random rand = new Random();
 
-        for (int i = 0; i <amount ; i++) {
+        for (int i = 0; i < amount; i++) {
             int randomNumber = rand.nextInt(Integer.MAX_VALUE);
             numberList.add(randomNumber);
         }
@@ -31,19 +34,28 @@ public class Main {
     }
 
 
-    public void selectionSort(ArrayList<Integer> list){
+    private ArrayList<Integer> selectionSort(ArrayList<Integer> list) {
+        for (int i = 0; i < list.size(); i++) {
 
-        for (int i = 0; i <list.size() ; i++) {
-
-            for (int j = i; j <list.size() ; j++) {
-                if(list.get(i)>list.get(j)){
+            for (int j = i; j < list.size(); j++) {
+                if (list.get(i) > list.get(j)) {
                     int temp = list.get(i);
-                    list.set(i,list.get(j));
-                    list.set(j,temp);
+                    list.set(i, list.get(j));
+                    list.set(j, temp);
                 }
             }
         }
-        System.out.println(list);
-
+        return list;
     }
+
+
+    private boolean isSorted(ArrayList<Integer> list)
+    {
+        boolean sorted = true;
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i-1).compareTo(list.get(i)) > 0) sorted = false;
+        }
+        return sorted;
+    }
+
 }
