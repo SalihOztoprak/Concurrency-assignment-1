fe9faec4ce659695126eab156c59a1c19db591a4
thomastunc <thomastunc@hotmail.com>
2018-09-05 14:34:06 +0200

    Merge branch 'master' of https://github.com/SalihOztoprak/Concurrency-opdracht-1
    # Conflicts:
    #	src/com/company/Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,25 +4,30 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Main {
+    private long timeTaken = 0;
 
     public static void main(String[] args) {
         new Main().run();
 
     }
 
-    public void run(){
-        ArrayList<Integer>randomList = new ArrayList<>();
-        randomList=generateNumbers(25000);
-        selectionSort(randomList);
-        System.out.println(randomList.size());
+    public void run() {
+        ArrayList<Integer> randomList;
+        randomList = generateNumbers(25000);
+        randomList = selectionSort(randomList);
+
+        System.out.println(randomList);
+        System.out.println("Size of list is: " + randomList.size());
+
+        System.out.println("Time taken in ms: " + timeTaken);
     }
 
 
-    private ArrayList<Integer> generateNumbers(int amount){
+    private ArrayList<Integer> generateNumbers(int amount) {
         ArrayList<Integer> numberList = new ArrayList<>();
         Random rand = new Random();
 
-        for (int i = 0; i <amount ; i++) {
+        for (int i = 0; i < amount; i++) {
             int randomNumber = rand.nextInt(Integer.MAX_VALUE);
             numberList.add(randomNumber);
         }
@@ -31,19 +36,20 @@ public class Main {
     }
 
 
-    public void selectionSort(ArrayList<Integer> list){
+    public ArrayList<Integer> selectionSort(ArrayList<Integer> list) {
+        final long startTime = System.currentTimeMillis();
+        for (int i = 0; i < list.size(); i++) {
 
-        for (int i = 0; i <list.size() ; i++) {
-
-            for (int j = i; j <list.size() ; j++) {
-                if(list.get(i)>list.get(j)){
+            for (int j = i; j < list.size(); j++) {
+                if (list.get(i) > list.get(j)) {
                     int temp = list.get(i);
-                    list.set(i,list.get(j));
-                    list.set(j,temp);
+                    list.set(i, list.get(j));
+                    list.set(j, temp);
                 }
             }
         }
-        System.out.println(list);
-
+        final long endTime = System.currentTimeMillis();
+        timeTaken = endTime-startTime;
+        return list;
     }
 }
