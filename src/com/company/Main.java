23f06834f3dd1c1016be0f5a33bdf84de068edcd
Salih Oztoprak <salih.58@live.nl>
2018-09-05 14:12:46 +0200

    Begin phase assignment 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,18 +6,41 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        
+        new Main().run();
 
     }
-    public ArrayList<Integer> generateNumbers(int amount){
+
+    public void run(){
+        selectionSort(generateNumbers(25000));
+    }
+
+
+    private ArrayList<Integer> generateNumbers(int amount){
         ArrayList<Integer> numberList = new ArrayList<>();
+        Random rand = new Random();
 
         for (int i = 0; i <amount ; i++) {
-            Random rand = new Random();
             int randomNumber = rand.nextInt(Integer.MAX_VALUE);
             numberList.add(randomNumber);
         }
 
         return numberList;
+    }
+
+
+    public void selectionSort(ArrayList<Integer> list){
+
+        for (int i = 0; i <list.size() ; i++) {
+
+            for (int j = i; j <list.size() ; j++) {
+                if(list.get(i)>list.get(j)){
+                    int temp = list.get(i);
+                    list.set(i,list.get(j));
+                    list.set(j,temp);
+                }
+            }
+        }
+        System.out.println(list);
+
     }
 }
