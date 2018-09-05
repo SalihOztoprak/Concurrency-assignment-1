7a634fb81bcb567049cb512627e32e4b56a8cec7
Salih Oztoprak <salih.58@live.nl>
2018-09-05 22:40:27 +0200

    updated the code, added a new class for generating numbers and sorting it

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,6 @@
 package com.company;
 
 import java.util.ArrayList;
-import java.util.Random;
 
 public class Main {
     private long timeTaken = 0;
@@ -12,53 +11,16 @@ public class Main {
     }
 
     public void run() {
+        NumberGenerator ng = new NumberGenerator();
+
         ArrayList<Integer> randomList;
-        randomList = generateNumbers(25000);
-        randomList = selectionSort(randomList);
-
+        randomList = ng.generateNumbers(25000);
+        randomList = ng.selectionSort(randomList);
         System.out.println(randomList);
+
         System.out.println("Size of list is: " + randomList.size());
-        System.out.println("Is the list sorted: " + isSorted(randomList));
-        System.out.println("Time taken in ms: " + timeTaken);
+        System.out.println("Is the list sorted: " + ng.isSorted(randomList));
+        System.out.println("Time taken in ms: " + ng.getTimeTaken());
     }
 
-
-    private ArrayList<Integer> generateNumbers(int amount) {
-        ArrayList<Integer> numberList = new ArrayList<>();
-        Random rand = new Random();
-
-        for (int i = 0; i < amount; i++) {
-            int randomNumber = rand.nextInt(Integer.MAX_VALUE);
-            numberList.add(randomNumber);
-        }
-
-        return numberList;
-    }
-
-
-    public ArrayList<Integer> selectionSort(ArrayList<Integer> list) {
-        final long startTime = System.currentTimeMillis();
-        for (int i = 0; i < list.size(); i++) {
-            int minIndex = i;
-            for (int j = i + 1; j < list.size(); j++) {
-                if (list.get(j) < list.get(minIndex)) {
-                    minIndex = j;
-                }
-            }
-            int temp = list.get(i);
-            list.set(i, list.get(minIndex));
-            list.set(minIndex, temp);
-        }
-        final long endTime = System.currentTimeMillis();
-        timeTaken = endTime - startTime;
-        return list;
-    }
-
-    private boolean isSorted(ArrayList<Integer> list) {
-        boolean sorted = true;
-        for (int i = 1; i < list.size(); i++) {
-            if (list.get(i - 1).compareTo(list.get(i)) > 0) sorted = false;
-        }
-        return sorted;
-    }
 }
---
+++ src/com/company/NumberGenerator.java
@@ -0,0 +1,57 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class NumberGenerator {
+    private long timeTaken = 0;
+
+    public NumberGenerator() {
+
+    }
+
+
+    public ArrayList<Integer> generateNumbers(int amount) {
+        ArrayList<Integer> numberList = new ArrayList<>();
+        Random rand = new Random();
+
+        for (int i = 0; i < amount; i++) {
+            int randomNumber = rand.nextInt(Integer.MAX_VALUE);
+            numberList.add(randomNumber);
+        }
+
+        return numberList;
+    }
+
+
+    public ArrayList<Integer> selectionSort(ArrayList<Integer> list) {
+
+        final long startTime = System.currentTimeMillis();
+        for (int i = 0; i < list.size(); i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(j) < list.get(minIndex)) {
+                    minIndex = j;
+                }
+            }
+            int temp = list.get(i);
+            list.set(i, list.get(minIndex));
+            list.set(minIndex, temp);
+        }
+        final long endTime = System.currentTimeMillis();
+        timeTaken = endTime - startTime;
+        return list;
+    }
+
+    public boolean isSorted(ArrayList<Integer> list) {
+        boolean sorted = true;
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i - 1).compareTo(list.get(i)) > 0) sorted = false;
+        }
+        return sorted;
+    }
+
+    public long getTimeTaken() {
+        return timeTaken;
+    }
+}
