360e9b5c0c33948f95d610ed14c8939b1563cd4c
thomastunc <thomastunc@hotmail.com>
2018-09-05 14:15:46 +0200

    Test 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,30 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
 public class Main {
-
     public static void main(String[] args) {
-        
-
+        new Main().run();
     }
-    public ArrayList<Integer> generateNumbers(int amount){
-        ArrayList<Integer> numberList = new ArrayList<>();
 
-        for (int i = 0; i <amount ; i++) {
-            Random rand = new Random();
-            int randomNumber = rand.nextInt(Integer.MAX_VALUE);
+    private void run() {
+        ArrayList<Integer> listOfNumbers = new ArrayList<>();
+
+        listOfNumbers = generateNumbers(25000);
+    }
+
+    private ArrayList<Integer> generateNumbers(int amount) {
+        ArrayList<Integer> numberList = new ArrayList<>();
+        Random random = new Random();
+
+        for (int i = 0; i < amount; i++) {
+            int randomNumber = random.nextInt(3);
             numberList.add(randomNumber);
         }
 
         return numberList;
     }
+
 }
