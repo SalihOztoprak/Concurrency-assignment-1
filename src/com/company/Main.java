2db2a71b9b5b1c4981ea6dbd4da8cf1b2cc774d9
thomastunc <thomastunc@hotmail.com>
2018-09-05 16:46:22 +0200

    Cleaned up the code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,23 +11,25 @@ public class Main {
 
     }
 
-    public void run() {
+    private void run() {
         ArrayList<Integer> randomList;
-        randomList = generateNumbers(25000);
+        int amount = 400000;
+
+        randomList = generateNumbers(amount);
         randomList = selectionSort(randomList);
 
         System.out.println(randomList);
         System.out.println("Size of list is: " + randomList.size());
-
+        System.out.println("Is the list sorted: " + isSorted(randomList));
         System.out.println("Time taken in ms: " + timeTaken);
     }
 
 
-    private ArrayList<Integer> generateNumbers(int amount){
+    private ArrayList<Integer> generateNumbers(int amount) {
         ArrayList<Integer> numberList = new ArrayList<>();
         Random rand = new Random();
 
-        for (int i = 0; i <amount ; i++) {
+        for (int i = 0; i < amount; i++) {
             int randomNumber = rand.nextInt(Integer.MAX_VALUE);
             numberList.add(randomNumber);
         }
@@ -36,28 +38,27 @@ public class Main {
     }
 
 
-    public ArrayList<Integer> selectionSort(ArrayList<Integer> list) {
+    private ArrayList<Integer> selectionSort(ArrayList<Integer> list) {
         final long startTime = System.currentTimeMillis();
         for (int i = 0; i < list.size(); i++) {
 
-            for (int j = i; j <list.size() ; j++) {
-                if(list.get(i)>list.get(j)){
+            for (int j = i; j < list.size(); j++) {
+                if (list.get(i) > list.get(j)) {
                     int temp = list.get(i);
-                    list.set(i,list.get(j));
-                    list.set(j,temp);
+                    list.set(i, list.get(j));
+                    list.set(j, temp);
                 }
             }
         }
         final long endTime = System.currentTimeMillis();
-        timeTaken = endTime-startTime;
+        timeTaken = endTime - startTime;
         return list;
     }
 
-    private boolean isSorted(ArrayList<Integer> list)
-    {
+    private boolean isSorted(ArrayList<Integer> list) {
         boolean sorted = true;
         for (int i = 1; i < list.size(); i++) {
-            if (list.get(i-1).compareTo(list.get(i)) > 0) sorted = false;
+            if (list.get(i - 1).compareTo(list.get(i)) > 0) sorted = false;
         }
         return sorted;
     }
